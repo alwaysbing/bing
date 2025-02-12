8e2b902e8a261ccb215e512f491345d326bad9ed
songnaeun <naeun@hangloo.co.kr>
2025-02-12 11:37:10 +0900

    이미지, 영상 수정

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,7 +9,7 @@ export default function Video(){
     return(
         <div className="video_wrapper">
             <div className="video_container">
-            <ReactPlayer className="react-player" url='https://www.youtube.com/watch?v=dYpXbsXWulw&list=PL35g5_2TtRUJZ74TtsUdVdk_KNmpvLfTh'
+            <ReactPlayer className="react-player" url='https://www.youtube.com/watch?v=z6kpJb08m8s&list=PLDBIIAx87PniXifFuOfZyt8PHKvmt1nq0'
                 width='100%'
                 playing muted loop config={{youtube: {
                     playerVars: { controls: 0,  start:0 }
